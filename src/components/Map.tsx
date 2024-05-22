93feaaad0fa97685d4a9d77b3901006a53b02058
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-05-22 00:48:15 -0300

    feat: adjust markers on map and add polyline

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,28 @@
-import MapView, { MapViewProps, PROVIDER_GOOGLE, LatLng, Marker } from "react-native-maps";
+import { useRef } from 'react'
+import MapView, { MapViewProps, PROVIDER_GOOGLE, LatLng, Marker, Polyline } from "react-native-maps";
 import { IconBox } from "./IconBox";
-import { Car } from "phosphor-react-native";
+import { Car, FlagCheckered } from "phosphor-react-native";
 
 type Props = MapViewProps & {
   coordinates: LatLng[];
 }
 
 export function Map({ coordinates, ...rest }: Props) {
+  const mapRef = useRef<MapView>(null);
 
   const lastCoordinate = coordinates[coordinates.length - 1];
 
+  async function onMapLoaded() {
+    if (coordinates.length > 1) {
+      mapRef.current?.fitToSuppliedMarkers(['departure', 'arrival'], {
+        edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+      })
+    }
+  }
+
   return (
     <MapView
+      ref={mapRef}
       provider={PROVIDER_GOOGLE}
       style={{ width: '100%', height: 200 }}
       region={{
@@ -20,11 +31,27 @@ export function Map({ coordinates, ...rest }: Props) {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005
       }}
+      onMapLoaded={onMapLoaded}
       {...rest}
     >
-      <Marker coordinate={coordinates[0]}>
+      <Marker identifier='departure' coordinate={coordinates[0]}>
         <IconBox size="SMALL" icon={Car} />
       </Marker>
+
+      {
+        coordinates.length > 1 &&
+        <>
+        <Marker identifier="arrival" coordinate={lastCoordinate}>
+          <IconBox size="SMALL" icon={FlagCheckered} />
+        </Marker>
+
+        <Polyline 
+          coordinates={[...coordinates]}
+          strokeColor='#29292E'
+          strokeWidth={7}
+        />
+      </>
+      }
     </MapView>
   );
 }
